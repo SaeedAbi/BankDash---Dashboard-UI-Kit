c6807bedfce30f48a016e85772951394ec78a5ca
SaeedAbi <abdary.saeed@gmail.com>
2024-04-21 15:23:00 +0330

    🔧 config tailwind fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,14 @@
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
     theme: {
-        // screens: {
-        //     sm: '480px',
-        //     md: '768px',
-        //     lg: '976px',
-        //     xl: '1440px',
-        // },
         colors: {
             'blue': '#1814F3',
-
         },
-        fontFamily: {
-            // sans: ['Graphik', 'sans-serif'],
-            // serif: ['Merriweather', 'serif'],
-        },
+        extend: {
+            fontFamily: {
+                'inter': ["Inter"],
+            },
+        }
     },
   plugins: [],
 }
