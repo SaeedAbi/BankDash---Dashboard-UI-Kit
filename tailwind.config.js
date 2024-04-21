cb2c41d707612afd0831a537c663c098cfcadffc
SaeedAbi <abdary.saeed@gmail.com>
2024-04-21 15:59:40 +0330

    🔧 config typography tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
 /** @type {import('tailwindcss').Config} */
- export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-    theme: {
-        colors: {
-            'blue': '#1814F3',
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontSize: {
+      h1: [
+        "24px",
+        {
+          lineHeight: "36px",
         },
-        extend: {
-            fontFamily: {
-                'inter': ["Inter"],
-            },
-        }
+      ],
+      h2: ["18px", { lineHeight: "30px" }],
+      p1: [
+        "16px",
+        {
+          lineHeight: "24px",
+        },
+      ],
+      p2: [
+        "14px",
+        {
+          lineHeight: "20px",
+        },
+      ],
+      s1: [
+        "12px",
+        {
+          lineHeight: "18px",
+        },
+      ],
+      s2: [
+        "10px",
+        {
+          lineHeight: "16px",
+        },
+      ],
     },
+    colors: {
+      blue: "#1814F3",
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
+  },
   plugins: [],
-}
-
+};
