08915eaac27983128b80dc9b8677de2f63aa6583
SaeedAbi <abdary.saeed@gmail.com>
2024-04-22 12:41:32 +0330

    🚧 creating breaking point

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./Styles/global.css";
+import Testam from "./Components/testam.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
