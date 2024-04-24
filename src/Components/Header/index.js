2c7c6a1f555b1ee149a9e9db771b212178398814
SaeedAbi <abdary.saeed@gmail.com>
2024-04-24 12:45:49 +0330

    ✨ ️ add header and side to components

---
+++ src/Components/Header/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Header = ({ title }) => {
+  return (
+    <div>
+      <div>{title}</div>
+    </div>
+  );
+};
+export default Header;
---
+++ src/Components/Side/index.js
@@ -0,0 +1,10 @@
+import react from "react";
+
+const Side = ({ title }) => {
+  return (
+    <div>
+      <div>{title}</div>
+    </div>
+  );
+};
+export default Side;
