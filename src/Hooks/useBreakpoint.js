18cc18833d780249fa98994469232abbe5fec780
SaeedAbi <abdary.saeed@gmail.com>
2024-04-24 17:37:23 +0330

    ⚡️ ✨use breakpoint hook

--- src/Hooks/useBreakpoint.js
+++ src/Hooks/useBreakpoint.js
@@ -1,7 +1,7 @@
 import react, { useEffect } from "react";
 
 export const useBreakpoint = () => {
-  const [tailwindSize, setTailwindSize] = react.useState([]);
+  const [tailwindSize, setTailwindSize] = react.useState("");
   const sizeChanges = () => {
     const width = window.innerWidth;
     let size = [];
@@ -10,11 +10,11 @@ export const useBreakpoint = () => {
     //'lg': '1024px',
     //'xl': '1280px',
     //'2xl': '1536px',
-    if (width < 640) size.push("sm");
-    else if (width < 768) size.push("md");
-    else if (width < 1024) size.push("xl");
-    else if (width < 1536) size.push("2xl");
-    setTailwindSize(size);
+    if (width >= 640) setTailwindSize("sm");
+    else if (width >= 768) setTailwindSize("md");
+    else if (width >= 1024) setTailwindSize("lg");
+    else if (width >= 1280) setTailwindSize("xl");
+    else if (width >= 1536) setTailwindSize("xxl");
   };
   useEffect(() => {
     sizeChanges();
